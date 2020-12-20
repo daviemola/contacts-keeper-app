974014c2bec70772baa8401b0ea22f8a1447c4c7
david mola <daviemola@gmail.com>
2020-12-20 20:30:27 +0530

    post init commit

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 const app = express()
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to contact keeper api' }))
+// app.get('/', (req, res) => res.json({ msg: 'Welcome to contact keeper api' }))
 
 connectDB()
 
 //init middleware
 app.use(express.json({ extended: false }))
+
+if (process.env.NODE_ENV === 'production') {
+  //set statif folder
+  app.use(express.static(`client/build`))
+
+  app.get(`*`, (req, res) =>
+    res.sendFile(path.resolve(__dirname, `client`, `build`, `index.html`)),
+  )
+}
 
 // Define routes
 app.use('/api/users', require('./routes/users'))
